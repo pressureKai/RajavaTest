e40a0b61c99e3ec140c98e0d3e0d4b6017a63ee6
pressureKai <2466129279@qq.com>
2021-02-26 22:59:26 +0800

    edit MainActivity.kt

--- app/src/main/java/com/kai/rajavatest/activity/MainActivity.kt
+++ app/src/main/java/com/kai/rajavatest/activity/MainActivity.kt
@@ -23,7 +23,8 @@ class MainActivity : AppCompatActivity() {
                     startActivity(intent)
                 }
                 "操作方法" -> {
-                    
+                    val intent = Intent(this, RxjavaBaseClassActivity::class.java)
+                    startActivity(intent)
                 }
                 "实用小技巧" -> {
                     
